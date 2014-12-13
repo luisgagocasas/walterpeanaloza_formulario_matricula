e2d62a32fb6f4301ef425f8d7ee04ec659854b09
Luis Gago <luisgago@lagc-peru.com>
2014-12-13 15:07:51 -0500

    init

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		//Preprocesador css
+        stylus: {
+            compile: {
+                options: {
+                    compress: true,
+                    linenos: false
+                },
+                files: [{
+                    'css/estilos.css': 'stylus/*.styl',
+                }]
+            }
+        },
+		// Compresor de .js
+	    /*uglify: {
+			options: {
+				mangle: false,
+				compress: {
+					drop_console: true
+				}
+			},
+			js: {
+				files: {
+					'js/main.min.js': 'javascript/*.js',
+				}
+			}
+	    },*/
+	    // Compilar Jade
+		/*jade: {
+            compile: {
+                options: {
+                    client: false,
+                    pretty: true
+                },
+                files: {
+					'index.html': 'jade/index.jade',
+				}
+            }
+        },*/
+        //Observar cambios
+		watch: {
+			options: {
+				nospawn: true,
+				livereload: true
+			},
+			//observar de stylus
+			stylesheets: {
+				files: ['stylus/*.styl'],
+				tasks: ['stylus']
+			},
+			//observar de js
+			/*scripts: {
+                files: ['javascript/*.js'],
+                tasks: ['uglify']
+            },*/
+            //observar el jade
+			/*jade: {
+				files: ["jade/*.jade"],
+				tasks: ["jade"]
+			}*/
+		},
+	});
+
+	//Cargo las tareas
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	//grunt.loadNpmTasks('grunt-contrib-uglify');
+	//grunt.loadNpmTasks('grunt-contrib-jade');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	// Run Default task(s).
+	grunt.registerTask('default', ['stylus','watch']);
+};
